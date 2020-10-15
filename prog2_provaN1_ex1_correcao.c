5b928f98addfb234ac5cc6e1dd2b674d7d6201b3
Kezia Campos <yourGitHub@email.com>
2020-10-14 22:29:27 -0300

    atualização

--- prog2_provaN1_ex1_correcao.c
+++ prog2_provaN1_ex1_correcao.c
@@ -1,3 +1,26 @@
+/*
+O programa deverá obter o Telefone (com o DDD), Endereço de entrega,
+Sabor da pizza e o se deseja ou não o refrigerante. Para cada pedido,
+o programa deverá calcular e exibir o valor a pagar com base na tabela
+de preços abaixo. Ao final do expediente, o programa deverá exibir o
+total de pedidos e o total do faturamento.
+Muzzarela  R$ 20,00
+Portuguesa  R$ 25,00
+Camarão  R$ 35,00
+Refrigerante  R$ 5,00
+O programa deverá obter os dados da seguinte forma:
+Telefone com DDD (apenas números):
+Endereço de Entrega:
+Sabor da Pizza [Muzzarela, Portuguesa ou Camarão]:
+Refrigerante? [S/N]:
+Obs1: O programa deverá ser finalizado ao digitar 0 (zero) no telefone.
+Obs2: O programa deverá conter 2 funções: uma função para calcular e
+retornar o valor a pagar e outra para contabilizar a quantidade de
+pedidos e o total do faturamento.
+Obs3: O sabor da pizza deverá ser digitado, exemplo: “Muzzarela”. Favor
+não troque a opção da escolha do sabor por números, exemplo:1-Muzzarela.
+*/
+
 //ex. 1 da prova de Prog 2
 #include<stdio.h>
 #include<string.h>
--- prog2_provaN1_ex2_correcao.c
+++ prog2_provaN1_ex2_correcao.c
@@ -1,3 +1,13 @@
+/*
+Faça um programa em C para registrar as doações realizadas no evento
+beneficente Baile da 3ª idade. Os dados a serem registrados são: Nome,
+Sexo e Valor da doação. (3,0)
+Ao final das inscrições o programa deverá exibir:
+a) A quantidade de pessoas que doaram acima de 50 reais.
+b) A média do valor da doação por pessoa.
+Obs.: O evento será "fechado" e terá a participação de apenas 150
+convidados. 
+*/
 //ex. 2 da prova de Prog 2
 #include<stdio.h>
 #include<string.h>
